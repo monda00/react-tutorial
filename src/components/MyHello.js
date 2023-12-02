47f0e5e1f64bab7b9e2d152fcbe6c0e70c358f1c
monda00 <monda0524@gmail.com>
2023-12-02 22:44:05 +0900

    add component

---
+++ src/components/MyHello.js
@@ -0,0 +1,3 @@
+export default function MyHello(props) {
+  return <div>Hello {props.myName}</div>;
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import MyHello from "./components/MyHello";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <MyHello myName="monda" />
   </React.StrictMode>
 );
 
