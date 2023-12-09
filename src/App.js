d76a93c59b1da37b11820c6bd760ee1768069515
monda00 <monda0524@gmail.com>
2023-12-09 21:39:36 +0900

    add StateBasic

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
 import MyHello from "./components/MyHello";
 import EventBasic from "./components/EventBasic";
+import StateBasic from "./components/StateBasic";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <MyHello myName="monda" />
       <EventBasic type="time" />
+      <StateBasic init={0} />
     </div>
   );
 }
